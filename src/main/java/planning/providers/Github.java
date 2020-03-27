8b252a9dbbc2fff25b6cc494c2b60533a0ca7767
Mårten Gustafson <marten.gustafson@gmail.com>
2020-03-27 18:28:50 +0100

    Reformat for the new decade.

--- src/main/java/planning/providers/Github.java
+++ src/main/java/planning/providers/Github.java
@@ -1,21 +1,5 @@
 package planning.providers;
 
-import io.dropwizard.auth.Auth;
-import org.eclipse.egit.github.core.client.IGitHubConstants;
-import planning.resources.OAuth;
-
-import java.net.URI;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.ext.Provider;
-
-import org.eclipse.egit.github.core.client.GitHubClient;
-
 import com.google.common.base.Optional;
 import com.sun.jersey.api.core.HttpContext;
 import com.sun.jersey.api.model.Parameter;
@@ -24,49 +8,67 @@ import com.sun.jersey.core.spi.component.ComponentScope;
 import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
 import com.sun.jersey.spi.inject.Injectable;
 import com.sun.jersey.spi.inject.InjectableProvider;
+import io.dropwizard.auth.Auth;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.IGitHubConstants;
+import planning.resources.OAuth;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.ext.Provider;
+import java.net.URI;
 
 @Provider
 public class Github implements InjectableProvider<Auth, Parameter> {
-    private final String githubHostname;
+  private final String githubHostname;
 
-    @Context
-    private HttpServletRequest request;
+  @Context private HttpServletRequest request;
 
-    public Github(final String githubHostname) {
-        this.githubHostname = githubHostname;
+  public Github(final String githubHostname) {
+    this.githubHostname = githubHostname;
+  }
+
+  @Override
+  public ComponentScope getScope() {
+    return ComponentScope.PerRequest;
+  }
+
+  @Override
+  public Injectable<GitHubClient> getInjectable(
+      final ComponentContext ic, final Auth a, final Parameter c) {
+    return new GithubInjectable(this.request);
+  }
+
+  private final class GithubInjectable extends AbstractHttpContextInjectable<GitHubClient> {
+    private final HttpServletRequest request;
+
+    public GithubInjectable(final HttpServletRequest request) {
+      this.request = request;
     }
 
     @Override
-    public ComponentScope getScope() {
-        return ComponentScope.PerRequest;
+    public GitHubClient getValue(final HttpContext c) {
+      final Optional<String> token = readToken(this.request.getSession(false));
+      if (token.isPresent()) {
+        final GitHubClient client =
+            new GitHubClient(
+                (IGitHubConstants.HOST_DEFAULT.equals(Github.this.githubHostname) ? "api." : "")
+                    + Github.this.githubHostname);
+        client.setOAuth2Token(token.get());
+        return client;
+      }
+      final URI login = UriBuilder.fromResource(OAuth.class).build();
+      throw new WebApplicationException(Response.temporaryRedirect(login).build());
     }
 
-    @Override
-    public Injectable<GitHubClient> getInjectable(final ComponentContext ic, final Auth a, final Parameter c) {
-        return new GithubInjectable(this.request);
+    private Optional<String> readToken(final HttpSession session) {
+      return (session == null)
+          ? Optional.absent()
+          : Optional.fromNullable((String) session.getAttribute("token"));
     }
-
-    private final class GithubInjectable extends AbstractHttpContextInjectable<GitHubClient> {
-        private final HttpServletRequest request;
-
-        public GithubInjectable(final HttpServletRequest request) {
-            this.request = request;
-        }
-
-        @Override
-        public GitHubClient getValue(final HttpContext c) {
-            final Optional<String> token = readToken(this.request.getSession(false));
-            if(token.isPresent()) {
-                final GitHubClient client = new GitHubClient((IGitHubConstants.HOST_DEFAULT.equals(Github.this.githubHostname) ? "api.": "") + Github.this.githubHostname);
-                client.setOAuth2Token(token.get());
-                return client;
-            }
-            final URI login = UriBuilder.fromResource(OAuth.class).build();
-            throw new WebApplicationException(Response.temporaryRedirect(login).build());
-        }
-
-        private Optional<String> readToken(final HttpSession session) {
-            return (session == null) ? Optional.absent() : Optional.fromNullable((String)session.getAttribute("token"));
-        }
-    }
+  }
 }
--- src/main/java/planning/resources/Milestones.java
+++ src/main/java/planning/resources/Milestones.java
@@ -1,15 +1,21 @@
 package planning.resources;
 
-import static java.util.Collections.emptySet;
-import static java.util.Collections.synchronizedSet;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.MediaType.TEXT_HTML;
-
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.views.View;
+import org.eclipse.egit.github.core.Milestone;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.MilestoneService;
+import org.eclipse.egit.github.core.service.UserService;
 import planning.views.IndexView;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
@@ -21,83 +27,83 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-
-import org.eclipse.egit.github.core.Milestone;
-import org.eclipse.egit.github.core.User;
-import org.eclipse.egit.github.core.client.GitHubClient;
-import org.eclipse.egit.github.core.service.IssueService;
-import org.eclipse.egit.github.core.service.MilestoneService;
-import org.eclipse.egit.github.core.service.UserService;
-
-import com.google.common.collect.ImmutableMap;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.synchronizedSet;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_HTML;
 
 @Path("/")
 @Produces(TEXT_HTML)
 public class Milestones {
 
-    private final List<String> repositories;
-    private final String githubHostname;
+  private final List<String> repositories;
+  private final String githubHostname;
 
-    public Milestones(final List<String> repositories, final String githubHostname) {
-        this.repositories = repositories;
-        this.githubHostname = githubHostname;
-    }
+  public Milestones(final List<String> repositories, final String githubHostname) {
+    this.repositories = repositories;
+    this.githubHostname = githubHostname;
+  }
 
-    @GET
-    public View index(@Auth final GitHubClient client) throws IOException {
-        final UserService users = new UserService(client);
-        return new IndexView(users.getUser(), this.repositories, this.githubHostname);
-    }
+  private static Map<String, Object> merge(
+      final Milestone milestone,
+      final String org,
+      final String repo,
+      final Map<Integer, Set<Map<String, String>>> assignees) {
+    return ImmutableMap.<String, Object>builder()
+        .put("organisation", org)
+        .put("repository", repo)
+        .put("id", UUID.randomUUID().toString())
+        .put("url", milestone.getUrl())
+        .put("title", milestone.getTitle())
+        .put("number", milestone.getNumber())
+        .put("openIssues", milestone.getOpenIssues())
+        .put("closedIssues", milestone.getClosedIssues())
+        .put("description", Strings.nullToEmpty(milestone.getDescription()))
+        .put("slug", String.format("%s/%s/%d", org, repo, milestone.getNumber()))
+        .put(
+            "assignees",
+            Optional.ofNullable(assignees.get(milestone.getNumber())).orElse(emptySet()))
+        .build();
+  }
 
-    @GET
-    @Path("milestones/{org}/{repo}")
-    @Produces(APPLICATION_JSON)
-    public List<Map<String, Object>> milestones(@Auth final GitHubClient client,
-                                                @PathParam("org") final String org,
-                                                @PathParam("repo") final String repo) throws IOException {
-        final ConcurrentMap<Integer, Set<Map<String, String>>> assignees = new ConcurrentHashMap<>();
-        new IssueService(client)
-                .getIssues(org, repo, ImmutableMap.of("milestone", "*", "state", "open"))
-                .parallelStream()
-                .forEach((issue) -> {
-                    final User assignee = issue.getAssignee();
-                    if(assignee != null) {
-                        final int milestone = issue.getMilestone().getNumber();
-                        assignees.putIfAbsent(milestone, synchronizedSet(new HashSet<>()));
-                        // User doesn't implement equals/hashCode because a Set<User> had been too simple...
-                        assignees.get(milestone).add(ImmutableMap.of(
-                                "url", assignee.getUrl(),
-                                "avatarUrl", assignee.getAvatarUrl(),
-                                "login", assignee.getLogin()));
-                    }
-                });
-        return new MilestoneService(client)
-                .getMilestones(org, repo, "open")
-                .parallelStream()
-                .map(milestone -> merge(milestone, org, repo, assignees))
-                .collect(Collectors.toList());
-    }
+  @GET
+  public View index(@Auth final GitHubClient client) throws IOException {
+    final UserService users = new UserService(client);
+    return new IndexView(users.getUser(), this.repositories, this.githubHostname);
+  }
 
-    private static Map<String, Object> merge(final Milestone milestone,
-                                             final String org,
-                                             final String repo,
-                                             final Map<Integer, Set<Map<String, String>>> assignees) {
-        return ImmutableMap.<String, Object>builder()
-                .put("organisation", org)
-                .put("repository", repo)
-                .put("id", UUID.randomUUID().toString())
-                .put("url", milestone.getUrl())
-                .put("title", milestone.getTitle())
-                .put("number", milestone.getNumber())
-                .put("openIssues", milestone.getOpenIssues())
-                .put("closedIssues", milestone.getClosedIssues())
-                .put("description", Strings.nullToEmpty(milestone.getDescription()))
-                .put("slug", String.format("%s/%s/%d", org, repo, milestone.getNumber()))
-                .put("assignees", Optional.ofNullable(assignees.get(milestone.getNumber())).orElse(emptySet()))
-                .build();
-    }
+  @GET
+  @Path("milestones/{org}/{repo}")
+  @Produces(APPLICATION_JSON)
+  public List<Map<String, Object>> milestones(
+      @Auth final GitHubClient client,
+      @PathParam("org") final String org,
+      @PathParam("repo") final String repo)
+      throws IOException {
+    final ConcurrentMap<Integer, Set<Map<String, String>>> assignees = new ConcurrentHashMap<>();
+    new IssueService(client)
+        .getIssues(org, repo, ImmutableMap.of("milestone", "*", "state", "open"))
+        .parallelStream()
+        .forEach(
+            (issue) -> {
+              final User assignee = issue.getAssignee();
+              if (assignee != null) {
+                final int milestone = issue.getMilestone().getNumber();
+                assignees.putIfAbsent(milestone, synchronizedSet(new HashSet<>()));
+                // User doesn't implement equals/hashCode because a Set<User> had been too simple...
+                assignees
+                    .get(milestone)
+                    .add(
+                        ImmutableMap.of(
+                            "url", assignee.getUrl(),
+                            "avatarUrl", assignee.getAvatarUrl(),
+                            "login", assignee.getLogin()));
+              }
+            });
+    return new MilestoneService(client)
+        .getMilestones(org, repo, "open")
+        .parallelStream()
+        .map(milestone -> merge(milestone, org, repo, assignees))
+        .collect(Collectors.toList());
+  }
 }
