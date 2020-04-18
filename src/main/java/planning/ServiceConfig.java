71de57d181283658a39a22c06fa8a5193d0454fd
Mårten Gustafson <marten.gustafson@gmail.com>
2020-04-18 15:37:38 +0200

    Tidy up env var constants.

--- src/main/java/planning/ServiceConfig.java
+++ src/main/java/planning/ServiceConfig.java
@@ -1,26 +1,28 @@
 package planning;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class ServiceConfig {
-  public static final String HOSTNAME_ENV = "GITHUB_HOSTNAME";
-  public static final String DEFAULT_HOST = "github.com";
+  public static final String ENV_GITHUB_CLIENT_ID = "GITHUB_CLIENT_ID";
+  public static final String ENV_GITHUB_CLIENT_SECRET = "GITHUB_CLIENT_SECRET";
+  public static final String ENV_REPOSITORIES = "REPOSITORIES";
+  public static final String ENV_GITHUB_HOSTNAME = "GITHUB_HOSTNAME";
+  public static final String GITHUB_COM_HOST = "github.com";
 
-  @Value("${REPOSITORIES}")
+  @Value("${" + ENV_REPOSITORIES + '}')
   private String repositories;
 
-  @Value("${" + HOSTNAME_ENV + ":" + DEFAULT_HOST + '}')
+  @Value("${" + ENV_GITHUB_HOSTNAME + ":" + GITHUB_COM_HOST + '}')
   private String hostname;
 
   public String hostname() {
-    return requireNonNull(this.hostname).trim().isEmpty() ? DEFAULT_HOST : this.hostname;
+    return requireNonNull(this.hostname).trim().isEmpty() ? GITHUB_COM_HOST : this.hostname;
   }
 
   public List<String> repositories() {
